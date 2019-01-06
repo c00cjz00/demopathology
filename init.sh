3f9bf088a7177725ee4b89bf28c95c9d60051c73
c00cjz00 <summerhill001@gmail.com>
2019-01-06 11:05:14 +0800

    init

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ git config --global user.name "c00cjz00"
 git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
-#git rm -r datatables
+git rm -r img20181224
 #java -jar ../removeHistory/bfg-1.12.16.jar --delete-files *config*.php
 git add *
 git commit -m "init"
